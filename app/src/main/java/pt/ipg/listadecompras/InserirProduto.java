2020232b0b3a35a561b692531cabb845123e2267
Cristianoteixeira119988 <39622223+Cristianoteixeira119988@users.noreply.github.com>
2019-04-25 17:30:49 +0100

    Spinner com as categorias adicionado na opcao de inserir produto

--- app/src/main/java/pt/ipg/listadecompras/InserirProduto.java
+++ app/src/main/java/pt/ipg/listadecompras/InserirProduto.java
@@ -6,8 +6,16 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class InserirProduto extends AppCompatActivity {
+public class InserirProduto extends AppCompatActivity  implements AdapterView.OnItemSelectedListener {
+
+    //Spinner------------------------------1
+    private Spinner spinnercategorias;
+    //-------------------------------------1
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +24,24 @@ public class InserirProduto extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //Spinner--------------------------------------------------------2
+        spinnercategorias = (Spinner) findViewById(R.id.spinnerCategoria);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.Categorias, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnercategorias.setAdapter(adapter);
+        spinnercategorias.setOnItemSelectedListener(this);
 
     }
 
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String texto = parent.getItemAtPosition(position).toString();
+        Toast.makeText(this, texto, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+    //--------------------------------------------------------------------2
 }
