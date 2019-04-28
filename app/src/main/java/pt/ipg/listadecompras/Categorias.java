97dfa85463be6a1c2c96ad7ca95f2b4a18b7f288
Cristianoteixeira119988 <39622223+Cristianoteixeira119988@users.noreply.github.com>
2019-04-28 20:40:23 +0100

    Update da activity inserir categoria

--- app/src/main/java/pt/ipg/listadecompras/Categorias.java
+++ app/src/main/java/pt/ipg/listadecompras/Categorias.java
@@ -1,13 +1,20 @@
 package pt.ipg.listadecompras;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Categorias extends AppCompatActivity {
+
+    private Button inserircategoria;
+    private Button editarcategoria;
+    private Button eliminarcategoria;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +23,31 @@ public class Categorias extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        inserircategoria=(Button) findViewById(R.id.buttonInserirCategoria);
+        editarcategoria=(Button) findViewById(R.id.buttonEditarCategoria);
+        eliminarcategoria=(Button) findViewById(R.id.buttonEliminarCategoria);
+
+        inserircategoria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Categorias.this, getString(R.string.carregou_para_inserir_categoria), Toast.LENGTH_SHORT).show();
+                Intent outraactivity= new Intent(Categorias.this, InserirCategoria.class);
+            }
+        });
+        editarcategoria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Categorias.this, getString(R.string.carregou_para_editar_categoria), Toast.LENGTH_SHORT).show();
+                Intent outraactivity= new Intent(Categorias.this, EditarCategoria.class);
+            }
+        });
+        eliminarcategoria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Categorias.this, getString(R.string.carregou_para_eliminar_categoria), Toast.LENGTH_SHORT).show();
+                Intent outraactivity= new Intent(Categorias.this, EliminarCategoria.class);
+            }
+        });
 
     }
 
--- app/src/main/java/pt/ipg/listadecompras/InserirCategoria.java
+++ app/src/main/java/pt/ipg/listadecompras/InserirCategoria.java
@@ -6,8 +6,16 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class InserirCategoria extends AppCompatActivity {
+
+    private Button botaoinserircategoria;
+    private Button botaocancelar;
+    private EditText edittextnomedacategoria;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +23,35 @@ public class InserirCategoria extends AppCompatActivity {
         setContentView(R.layout.activity_inserir_categoria);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        botaocancelar=(Button) findViewById(R.id.buttonCancelar);
+        botaoinserircategoria=(Button) findViewById(R.id.buttonInserirCaategoria);
+        edittextnomedacategoria=(EditText) findViewById(R.id.EditTextNomeDaCategoria);
+
+        botaoinserircategoria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String mensagem = edittextnomedacategoria.getText().toString();
+                if (mensagem.trim().length() == 0){
+                    edittextnomedacategoria.setError(getString(R.string.nome_obrigatorio));
+                }else if (mensagem.length() <= 3){
+                    edittextnomedacategoria.setError(getString(R.string.numero_minimo_de_caracters));
+                }else if(mensagem.length() >= 25){
+                    edittextnomedacategoria.setError(getString(R.string.numero_maximo_de_caracters));
+                }else {
+                    Toast.makeText(InserirCategoria.this, getString(R.string.categoria_criada), Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+            }
+
+        });
+        botaocancelar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
 
     }
