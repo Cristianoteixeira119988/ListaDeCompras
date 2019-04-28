1d03d6c91edbe1ddb3c4507b84ae93191822fdd3
Cristianoteixeira119988 <39622223+Cristianoteixeira119988@users.noreply.github.com>
2019-04-28 20:55:06 +0100

    Update da activity editar categoria com todas as funcionalidades

--- app/src/main/java/pt/ipg/listadecompras/EditarCategoria.java
+++ app/src/main/java/pt/ipg/listadecompras/EditarCategoria.java
@@ -6,8 +6,14 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class EditarCategoria extends AppCompatActivity {
+    private Button botaoguardarcategoria;
+    private Button botaocancelar;
+    private EditText edittextnovonomedacategoria;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +22,36 @@ public class EditarCategoria extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        botaocancelar=(Button) findViewById(R.id.buttonCancelar);
+        botaoguardarcategoria=(Button) findViewById(R.id.buttonGuardarCategoria);
+        edittextnovonomedacategoria=(EditText) findViewById(R.id.EditTextNovoNomeCategoria);
 
+
+
+        botaoguardarcategoria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String mensagem = edittextnovonomedacategoria.getText().toString();
+                if (mensagem.trim().length() == 0){
+                    edittextnovonomedacategoria.setError(getString(R.string.nome_obrigatorio));
+                }else if (mensagem.length() <= 3){
+                    edittextnovonomedacategoria.setError(getString(R.string.numero_minimo_de_caracters));
+                }else if(mensagem.length() >= 25){
+                    edittextnovonomedacategoria.setError(getString(R.string.numero_maximo_de_caracters));
+                }else {
+                    Toast.makeText(EditarCategoria.this, getString(R.string.categoria_guardada), Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+            }
+
+        });
+        botaocancelar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
 }
