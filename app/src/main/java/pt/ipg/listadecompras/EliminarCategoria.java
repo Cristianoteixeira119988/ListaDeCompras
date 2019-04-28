720b5292d1d9e0d6bf21411560e6d3e58837902c
Cristianoteixeira119988 <39622223+Cristianoteixeira119988@users.noreply.github.com>
2019-04-28 21:06:29 +0100

    Update da activity eliminar categoria

--- app/src/main/java/pt/ipg/listadecompras/EliminarCategoria.java
+++ app/src/main/java/pt/ipg/listadecompras/EliminarCategoria.java
@@ -6,8 +6,13 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class EliminarCategoria extends AppCompatActivity {
+
+    private Button botaocancelar;
+    private Button botaoeliminarcategoria;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +21,20 @@ public class EliminarCategoria extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        botaocancelar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        botaoeliminarcategoria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(EliminarCategoria.this, getString(R.string.categoria_eliminada_com_sucesso), Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
 
     }
 
