790d3125803adba5fbb3a1ca6b74d195af29aeb7
Cristianoteixeira119988 <39622223+Cristianoteixeira119988@users.noreply.github.com>
2019-05-07 17:37:45 +0100

    Bdtableprodutos concluida

--- app/src/main/java/pt/ipg/listadecompras/BdTableProdutos.java
+++ app/src/main/java/pt/ipg/listadecompras/BdTableProdutos.java
@@ -1,4 +1,48 @@
 package pt.ipg.listadecompras;
 
-public class BdTableProdutos {
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.provider.BaseColumns;
+
+public class BdTableProdutos implements BaseColumns {
+
+    public static final String NOME_TABELA = "Produtos";
+    public static final String CAMPO_NOME_PRODUTO = "NomeProduto";
+    public static final String CAMPO_QUANTIDADE = "Quantidade";
+    public static final String CAMPO_CATEGORIA = "Categoria";
+    public static final String CAMPO_DATA_FALTOU = "DataQueFaltou";
+    private SQLiteDatabase db;
+
+    public BdTableProdutos(SQLiteDatabase db) {
+        this.db = db;
+    }
+
+    public void cria() {
+        db.execSQL(
+                "CREATE TABLE " + NOME_TABELA + "(" +
+                        _ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                        CAMPO_NOME_PRODUTO + " TEXT NOT NULL," +
+                        CAMPO_QUANTIDADE + " INTEGER NOT NULL," +
+                        CAMPO_CATEGORIA + " INTEGER NOT NULL," +
+                        "FOREIGN KEY (" + CAMPO_CATEGORIA + ") REFERENCES " + BdTableCategoria.NOME_TABELA + "(" + BdTableCategoria._ID + ")" +
+                        ")"
+        );
+    }
+
+    public Cursor query(String[] columns, String selection, String[] selectionArgs, String groupBy, String having, String orderBy) {
+        return db.query(NOME_TABELA, columns, selection, selectionArgs, groupBy, having, orderBy);
+    }
+
+    public long insert(ContentValues values) {
+        return db.insert(NOME_TABELA, null, values);
+    }
+
+    public int update(ContentValues values, String whereClause, String [] whereArgs) {
+        return db.update(NOME_TABELA, values, whereClause, whereArgs);
+    }
+
+    public int delete(String whereClause, String[] whereArgs) {
+        return db.delete(NOME_TABELA, whereClause, whereArgs);
+    }
 }
