39dbf12fc35024b78af91f34d995c37bf2786feb
Cristianoteixeira119988 <39622223+Cristianoteixeira119988@users.noreply.github.com>
2019-05-07 17:01:26 +0100

    dbtablecategoria concluido

--- app/src/main/java/pt/ipg/listadecompras/BdTableCategoria.java
+++ app/src/main/java/pt/ipg/listadecompras/BdTableCategoria.java
@@ -1,4 +1,42 @@
 package pt.ipg.listadecompras;
 
-public class BdTableCategoria {
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.provider.BaseColumns;
+
+public class BdTableCategoria implements BaseColumns {
+    public static final String NOME_TABELA = "Categorias";
+    public static final String NOME_CATEGORIA = "Nome";
+
+    private SQLiteDatabase db;
+
+    public BdTableCategoria(SQLiteDatabase db) {
+        this.db = db;
+    }
+
+    public void cria() {
+        db.execSQL(
+                "CREATE TABLE " + NOME_TABELA + "(" +
+                        _ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                        NOME_CATEGORIA + " TEXT NOT NULL" +
+                        ")"
+        );
+    }
+
+    public Cursor query(String[] columns, String selection, String[] selectionArgs, String groupBy, String having, String orderBy) {
+        return db.query(NOME_TABELA, columns, selection, selectionArgs, groupBy, having, orderBy);
+    }
+
+    public long insert(ContentValues values) {
+        return db.insert(NOME_TABELA, null, values);
+    }
+
+    public int update(ContentValues values, String whereClause, String [] whereArgs) {
+        return db.update(NOME_TABELA, values, whereClause, whereArgs);
+    }
+
+    public int delete (String whereClause, String[] whereArgs) {
+        return db.delete(NOME_TABELA, whereClause, whereArgs);
+    }
 }
