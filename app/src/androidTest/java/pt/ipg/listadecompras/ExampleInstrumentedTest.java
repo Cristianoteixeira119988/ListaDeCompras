153c8355417a9632e282bec385d90ecbb4a3cf48
Cristianoteixeira119988 <39622223+Cristianoteixeira119988@users.noreply.github.com>
2019-05-07 18:40:22 +0100

    Test criado

--- app/src/androidTest/java/pt/ipg/listadecompras/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/pt/ipg/listadecompras/ExampleInstrumentedTest.java
@@ -1,9 +1,11 @@
 package pt.ipg.listadecompras;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -16,11 +18,42 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+    @Before
+    public void apagaBaseDados() {
+        getAppContext().deleteDatabase(SqliteOpenHelper.NOME_BASE_DADOS);
+    }
 
-        assertEquals("pt.ipg.listadecompras", appContext.getPackageName());
+    @Test
+    public void criaBdLivros() {
+        // Context of the app under test.
+        Context appContext = getAppContext();
+
+        SqliteOpenHelper openHelper = new SqliteOpenHelper(appContext);
+
+        SQLiteDatabase db = openHelper.getReadableDatabase();
+
+        assertTrue(db.isOpen());
+    }
+
+    private Context getAppContext() {
+        return InstrumentationRegistry.getTargetContext();
+    }
+
+    @Test
+    public void testCRUD() {
+        SqliteOpenHelper openHelper = new SqliteOpenHelper(getAppContext());
+        SQLiteDatabase db = openHelper.getWritableDatabase();
+
+        BdTableCategoria tabelaCategorias = new BdTableCategoria(db);
+
+        Categoria categoria = new Categoria();
+        categoria.setNomecategoria("Mercearia");
+        long idMercearia = tabelaCategorias.insert(categoria.getContentValues());
+        assertNotEquals(-1, idMercearia);
+
+        categoria = new Categoria();
+        categoria.setNomecategoria("Bricolage");
+        long idBricolage = tabelaCategorias.insert(categoria.getContentValues());
+        assertNotEquals(-1, idBricolage);
     }
 }
