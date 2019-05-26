bfc84aa1e8fc92bc61d1401b2e77e244b5b6d203
Cristianoteixeira119988 <39622223+Cristianoteixeira119988@users.noreply.github.com>
2019-05-27 00:30:32 +0100

    Create cursor load

--- app/src/main/java/pt/ipg/listadecompras/MinhasListas.java
+++ app/src/main/java/pt/ipg/listadecompras/MinhasListas.java
@@ -1,18 +1,24 @@
 package pt.ipg.listadecompras;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MinhasListas extends AppCompatActivity {
+public class MinhasListas extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
-
+    private static final int ID_CURSO_LOADER_LISTAS = 0;
     private Button botaoeditarlista;
     private Button botaoapagarlista;
     private Button botaocriarlistaminhaslistas;
@@ -22,6 +28,8 @@ public class MinhasListas extends AppCompatActivity {
         setContentView(R.layout.activity_minhas_listas);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        getSupportLoaderManager().initLoader(ID_CURSO_LOADER_LISTAS, null, this);
 
         botaoeditarlista = (Button) findViewById(R.id.buttonEditarLista);
         botaoapagarlista = (Button) findViewById(R.id.buttonApagarLista);
@@ -57,4 +65,22 @@ public class MinhasListas extends AppCompatActivity {
 
     }
 
+    @NonNull
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, @Nullable Bundle bundle) {
+        CursorLoader cursorLoader = new CursorLoader(this, ListasContentProvider.ENDERECO_LISTAS, BdTableListas.TODAS_COLUNAS, null, null, BdTableListas.CAMPO_NOME_LISTA
+        );
+
+        return cursorLoader;
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor cursor) {
+
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+
+    }
 }
